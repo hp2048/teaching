dcf6b5f7ae5f05fb7a1e710ae420e8d95d66d6a8
Hardip Patel <patelhardip@gmail.com>
2020-08-19 15:26:51 +1000

    Update gccontent.R

--- gccontent.R
+++ gccontent.R
@@ -1,3 +1,18 @@
+##include central libraries in your paths
+.libPaths(c(.libPaths(), "/usr/local/lib/R/site-library", "/usr/lib/R/site-library", "/usr/lib/R/library")) 
+
+##load genome sequence library
+library(biovizBase)
+library(BSgeome)
+library(BSgenome.Hsapiens.UCSC.hg38) 
+library(GenomicRanges)
+
+hchrinfo <- data.frame(chr = Hsapiens@seqinfo@seqnames, start = 1, end = Hsapiens@seqinfo@seqlengths)
+myslidingwindows <- GenomicRanges::slidingWindows(GRanges(hchrinfo), width = 250000, step = 250000)
+myslidingwindows
+
+
+
 library(Biostrings)
 library(ggplot2)
 
